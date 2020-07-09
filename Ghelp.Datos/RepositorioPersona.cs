9eb674ccaab5591402215427fb9ac0794a6a3bda
Gabriel <Gabriel@DESKTOP-9VAIQ28>
2020-07-09 11:34:27 -0300

    Se agregan clases para separar codigo segun modelo MVC

---
+++ Ghelp.Datos/RepositorioPersona.cs
@@ -0,0 +1,21 @@
+﻿using Ghelp.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ghelp.Datos
+{
+    public class RepositorioPersona
+    {
+        public static List<Persona> Data = new List<Persona>
+        {
+            new Persona {Id=1, Nombre="prueba" }
+        };
+        public Persona ObtienePersona(int id)
+        {
+            return Data.FirstOrDefault(persona => persona.Id == id);
+        }
+    }
+}
---
+++ Ghelp.Logica/LogicaPersonas.cs
@@ -0,0 +1,27 @@
+﻿using Ghelp.Datos;
+using Ghelp.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ghelp.Logica
+{
+    public class LogicaPersonas
+    {
+        private RepositorioPersona _repositorioPersonas;
+
+        public LogicaPersonas()
+        {
+            _repositorioPersonas = new RepositorioPersona();
+
+        }
+
+
+        public Persona ObtienePersona(int id)
+        {
+            return _repositorioPersonas.ObtienePersona(id);
+        }
+    }
+}
